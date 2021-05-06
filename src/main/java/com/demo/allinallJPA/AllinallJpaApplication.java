854816e2a32c3c7efb1144abef5f1c5f6d77e4b5
Munir <info.mubir@gmail.com>
2021-05-07 00:54:29 +0900

    properties_&_maven

---
+++ src/main/java/com/demo/allinallJPA/AllinallJpaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.allinallJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllinallJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllinallJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/allinallJPA/AllinallJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.allinallJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllinallJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
